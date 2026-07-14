19f646c330b81ef676322e8db0800d48a705f8e3
Yashaswini-2824 <yashaswini@primordiusglobal.com>
2026-07-14 10:50:14 +0530

    Refactor holiday management logic to support updating existing holidays and improve date selection handling

--- server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a888970d908c93eb28b97ce2f1a9783bf04639d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5eddee5415a311cd45eea162de26c614df4414e")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
